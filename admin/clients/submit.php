583ad9904c2d8c24f681bbd2b966acbbb9bd19f3
Kelvin <kelvingraddick@kg.codes>
2026-03-04 02:15:04 -0500

    Add development email to admin panel client submit

--- admin/clients/submit.php
+++ admin/clients/submit.php
@@ -27,7 +27,7 @@
 			$time = date("YmdHis");
 			$id = mysqli_insert_id;
 			$admins = array(); 
-			$admins[0] = "('email', '$subject', '$body', '$time', 'new_user', '$id')";
+			$admins[0] = "('development@wavelinkllc.com', '$subject', '$body', '$time', 'new_user', '$id')";
 			if(mysqli_query("insert into emails (`to`, subject, text, time, type, reference_id) VALUES ".implode(',', $admins))) { } else { }
 			
 			header("Location: index.php");
