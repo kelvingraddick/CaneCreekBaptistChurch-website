1fbf50aa89578a9906727010b573a60a8c59cffb
Kelvin <kelvingraddick@kg.codes>
2026-03-04 02:13:25 -0500

    Fixed contact form

--- add_contact.php
+++ add_contact.php
@@ -1,4 +1,8 @@
 <?php
+	//error_reporting(E_ALL);
+	//ini_set('display_errors', '1');
+	//ini_set('display_startup_errors', '1');
+
 	include $_SERVER['DOCUMENT_ROOT'].'/utility/configuration.php';
 	include $_SERVER['DOCUMENT_ROOT'].'/utility/functions.php';
 	$c = connect_to_database();
@@ -36,39 +40,39 @@
 	}else{
 		if(mysqli_num_rows(mysqli_query($c, "SELECT email FROM users WHERE email = '$email' AND email <> 'none'")) ||
 		   mysqli_num_rows(mysqli_query($c, "SELECT phone FROM users WHERE phone = '$phone' AND phone <> 'none'"))) {
-			$id = mysqli_insert_id;
+			$id = mysqli_insert_id($c);
 			$time = date("YmdHis");
 			
 			$subject = "Repeat inquiry for ".$site_name;
 			$body = "Type: ".$type."<br />First Name: ".$first_name."<br />Last Name: ".$last_name."<br />Email: ".$email."<br />Phone: ".$phone."<br />Message: ".$message;
 			$headers = "MIME-Version: 1.0" . "\r\n";
 			$headers .= "Content-type:text/html;charset=UTF-8"."\r\n";
-			$headers .= 'From: "'.$site_name.' <'.$setting['email'].'>'."\r\n";
+			$headers .= 'From: "'.$site_name.'" <notifications@mail.wavelinkllc.com>'."\r\n";
 			mail($setting['email'],$subject,$body,$headers);
-			mail("email",$subject,$body,$headers);
+			mail("development@wavelinkllc.com",$subject,$body,$headers);
 			
 			echo '<img src="http://www.wavelinkllc.com/images/success.png" style="height:40px;" />&nbsp;&nbsp;We will contact you shortly as this email and/or phone number has <u>already been added to our email list</u>. <i>Thank you!</i>';
 		}else{
 			if(mysqli_query($c, "insert into users(first_name, last_name, email, phone, description) values('$first_name', '$last_name', '$email', '$phone', '$message')")){
-				$id = mysqli_insert_id;
+				$id = mysqli_insert_id($c);
 				$time = date("YmdHis");
 				
 				$subject = "New inquiry for ".$site_name;
 				$body = "Type: ".$type."<br />First Name: ".$first_name."<br />Last Name: ".$last_name."<br />Email: ".$email."<br />Phone: ".$phone."<br />Message: ".$message;
 				$headers = "MIME-Version: 1.0" . "\r\n";
 				$headers .= "Content-type:text/html;charset=UTF-8"."\r\n";
-				$headers .= 'From: "'.$site_name.'" <'.$setting['email'].'>'."\r\n";
+				$headers .= 'From: "'.$site_name.'" <notifications@mail.wavelinkllc.com>'."\r\n";
 				mail($setting['email'],$subject,$body,$headers);
-				mail("email",$subject,$body,$headers);
+				mail("development@wavelinkllc.com",$subject,$body,$headers);
 				
 				//Send email to user			
 				$subject = "Your \"".$type."\" request has been received by ".$site_name."!";
 				$body = "Thank you for your inquiry!<br />We will contact you ASAP and also keep you up-to-date on ".$site_name." news.<br />P.S. If you wish to be removed, please email ".$setting['email'];
 				$headers = "MIME-Version: 1.0" . "\r\n";
 				$headers .= "Content-type:text/html;charset=UTF-8"."\r\n";
-				$headers .= 'From: "'.$site_name.'" <'.$setting['email'].'>'."\r\n";
+				$headers .= 'From: "'.$site_name.'" <notifications@mail.wavelinkllc.com>'."\r\n";
 				mail($email,$subject,$body,$headers);
-				mail("email",$subject,$body,$headers);
+				mail("development@wavelinkllc.com",$subject,$body,$headers);
 		
 				echo '<img src="http://www.wavelinkllc.com/images/success.png" style="height:20px;" />&nbsp;&nbsp;We will contact you ASAP and we\'ve sent you an email. <i>Thank you!</i>';				
 			}else{ 
